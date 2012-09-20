77184d6ea691987b69351b3f889970016068636d
belldandy-syaorin <belldandy@vm-debian.(none)>
2012-09-20 06:42:20 -0400

    2012-09-20_00 update

---
+++ setting_libreoffice_empty.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ -f ~/.config/libreoffice/3/user/registrymodifications.xcu ] ; then
+	rm ~/.config/libreoffice/3/user/registrymodifications.xcu
+	echo "[ ok ] Find registrymodifications.xcu & Remove"
+else
+	echo "[info] Can't Find registrymodifications.xcu & Exit"
+fi
+exit 0
