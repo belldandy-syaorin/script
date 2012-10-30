484d19239f0771c25d549506f23ccfe91958e762
belldandy <belldandy@vm-debian.(none)>
2012-10-30 03:16:16 -0400

    2012-10-30_00 update

--- setting_flash_player.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -L /usr/lib/mozilla/plugins/libflashplayer.so ] ; then
-	echo "[info] Symbolic Link Exist & Exit"
-else
-	sudo ln -s /libflashplayer.so /usr/lib/mozilla/plugins
-	echo "[ ok ] Symbolic Link Create Success"
-fi
-exit 0
