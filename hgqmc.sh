f3c72dbb63f296190ad6c2f6c34384bfacfc9b45
Belldandy <Belldandy@Syaorin.com>
2014-10-30 03:52:47 +0900

    add hgqmc.sh

---
+++ hgqmc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+declare -a argument
+	argument[0]="$(hg status)"
+	argument[1]="'$(hg status|grep ^M|sed 's/^M/mod/g')'"
+if [ "${argument[0]}" = "" ] ; then
+	echo "[info] Repository Status : n/a"
+	exit 0
+fi
+if [ "${argument[1]}" = "''" ] ; then
+	echo "[info] Modified : n/a"
+	exit 0
+fi
+echo "[info] Repository Status :" "${argument[0]}"
+echo "[info] Commit Message    :" "${argument[1]}"
+read -t 5 -p "Commit = Y/y Or Exit = Other) : " ans
+checkans=$(echo "${ans:0:1}"|grep '^[[:alpha:]]')
+if [ "$checkans" = "Y" -o "$checkans" = "y" ] ; then
+	hg commit -m "${argument[1]}"
+	echo "[info] Commit"
+else
+	echo "[info] Exit"
+fi
+exit 0
