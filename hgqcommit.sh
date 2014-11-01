9c3db4159d388715c8c5bb605796f7c74718d9a2
Belldandy <Belldandy@Syaorin.com>
2014-11-01 14:35:29 +0900

    add hgqcommit.sh & mod hgqkdiff3.sh

---
+++ hgqcommit.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+declare -a argument
+	argument[0]="$(hg status)"
+	argument[1]="$(echo "${argument[0]}"|awk NR==1'{printf "%s\n",$1}')"
+	argument[2]="$(echo "${argument[0]}"|awk NR==2'{printf "%s\n",$1}')"
+if [ "${argument[0]}" = "" ] ; then
+	echo "[info] Repository Status : n/a"
+	exit 0
+fi
+if [ "${argument[2]}" != "" ] ; then
+	echo "[info] Added Or Modified : Multiple"
+	exit 0
+elif [ "${argument[1]}" = "A" ] ; then
+	argument[3]="$(hg status|grep ^A|sed 's/^A/mod/g')"
+elif [ "${argument[1]}" = "M" ] ; then
+	argument[3]="$(hg status|grep ^M|sed 's/^M/mod/g')"
+elif [ "${argument[1]}" = "" ] ; then
+	echo "[info] Added Or Modified : n/a"
+	exit 0
+fi
+echo "[info] Repository Status :" "${argument[0]}"
+echo "[info] Commit Message    :" "${argument[3]}"
+echo "[info] (After 5s Will Auto Exit)"
+read -t 5 -p "Commit = Y/y Or Exit = Other) : " ans
+checkans=$(echo "${ans:0:1}"|grep '^[[:alpha:]]')
+if [ "$checkans" = "Y" -o "$checkans" = "y" ] ; then
+	echo "[info] Commit"
+	hg commit -m "${argument[3]}"
+else
+	echo "[info] Exit"
+fi
+exit 0
--- hgqkdiff3.sh
+++ hgqkdiff3.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 declare -a argument
-	argument[0]="$(hg log --limit 2|grep changeset:|awk NR==1'{printf "%s\n",$2}')"
-	argument[1]="$(hg log --limit 2|grep changeset:|awk NR==2'{printf "%s\n",$2}')"
+	argument[0]="$(hg log --limit 2|grep changeset:)"
+	argument[1]="$(echo "${argument[0]}"|awk NR==1'{printf "%s\n",$2}')"
+	argument[2]="$(echo "${argument[0]}"|awk NR==2'{printf "%s\n",$2}')"
 if [ "${argument[0]}" = "" -o "${argument[1]}" = "" ] ; then
 	echo "[info] Repository Log : n/a"
 	exit 0
 fi
-echo "[info] Repository Revision(Last 2) :" "${argument[0]}" "${argument[1]}"
+echo "[info] Repository Revision(Last 2) :" "${argument[1]}" "${argument[2]}"
 echo "[info] (After 5s Will Auto Exit)"
 read -t 5 -p "Log(KDiff3) = Y/y Or Exit = Other) : " ans
 checkans=$(echo "${ans:0:1}"|grep '^[[:alpha:]]')
 if [ "$checkans" = "Y" -o "$checkans" = "y" ] ; then
 	echo "[info] Log(KDiff3)"
 	hg qlog
-	hg kdiff3 -r"${argument[0]}" -r"${argument[1]}"
+	hg kdiff3 -r"${argument[1]}" -r"${argument[2]}"
 else
 	echo "[info] Exit"
 fi
