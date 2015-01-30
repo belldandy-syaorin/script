cf02b17fb084ca7dcd9b1d302a38e3744d993b3e
Belldandy <Belldandy@Syaorin.com>
2015-01-30 13:40:30 +0900

    remove setting_libreoffice_empty.sh & setting_libreoffice_empty.vbs

--- setting_libreoffice_empty.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -f ~/.config/libreoffice/4/user/registrymodifications.xcu ] ; then
-	rm ~/.config/libreoffice/4/user/registrymodifications.xcu
-	echo "[ ok ] Find registrymodifications.xcu & Remove"
-else
-	echo "[info] Can't Find registrymodifications.xcu & Exit"
-fi
-exit 0
