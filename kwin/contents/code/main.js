eb53c7df128d0d33d52fadc3ce3fd37f834bbead
Belldandy <Belldandy@Syaorin.com>
2015-09-07 02:02:11 +0900

    mod main.js

--- kwin/contents/code/main.js
+++ kwin/contents/code/main.js
@@ -88,6 +88,70 @@ registerShortcut("kwin-wmplus(smartsize_large)",
 				}
 			}
 		});
+registerShortcut("kwin-wmplus(position1)",
+		"kwin-wmplus(position1)",
+		"Meta+Alt+1",
+		function() {
+			var clients = workspace.clientList();
+			for (var i=0; i<clients.length; i++) {
+				if(clients[i].active) {
+					activeClient = clients[i];
+					var workGeo = workspace.clientArea(KWin.PlacementArea, activeClient.screen, 1);
+					var geo = activeClient.geometry;
+					geo.x = 0;
+					geo.y = workGeo.height - geo.height;
+					activeClient.geometry = geo;
+				}
+			}
+		});
+registerShortcut("kwin-wmplus(position4)",
+		"kwin-wmplus(position4)",
+		"Meta+Alt+4",
+		function() {
+			var clients = workspace.clientList();
+			for (var i=0; i<clients.length; i++) {
+				if(clients[i].active) {
+					activeClient = clients[i];
+					var workGeo = workspace.clientArea(KWin.PlacementArea, activeClient.screen, 1);
+					var geo = activeClient.geometry;
+					geo.x = 0;
+					geo.y = (workGeo.height - geo.height) / 2;
+					activeClient.geometry = geo;
+				}
+			}
+		});
+registerShortcut("kwin-wmplus(position7)",
+		"kwin-wmplus(position7)",
+		"Meta+Alt+7",
+		function() {
+			var clients = workspace.clientList();
+			for (var i=0; i<clients.length; i++) {
+				if(clients[i].active) {
+					activeClient = clients[i];
+					var workGeo = workspace.clientArea(KWin.PlacementArea, activeClient.screen, 1);
+					var geo = activeClient.geometry;
+					geo.x = 0;
+					geo.y = 0;
+					activeClient.geometry = geo;
+				}
+			}
+		});
+registerShortcut("kwin-wmplus(position2)",
+		"kwin-wmplus(position2)",
+		"Meta+Alt+2",
+		function() {
+			var clients = workspace.clientList();
+			for (var i=0; i<clients.length; i++) {
+				if(clients[i].active) {
+					activeClient = clients[i];
+					var workGeo = workspace.clientArea(KWin.PlacementArea, activeClient.screen, 1);
+					var geo = activeClient.geometry;
+					geo.x = (workGeo.width - geo.width) / 2;
+					geo.y = workGeo.height - geo.height;
+					activeClient.geometry = geo;
+				}
+			}
+		});
 registerShortcut("kwin-wmplus(position5)",
 		"kwin-wmplus(position5)",
 		"Meta+Alt+5",
@@ -100,6 +164,70 @@ registerShortcut("kwin-wmplus(position5)",
 					var geo = activeClient.geometry;
 					geo.x = (workGeo.width - geo.width) / 2;
 					geo.y = (workGeo.height - geo.height) / 2;
+					activeClient.geometry = geo;
+				}
+			}
+		});
+registerShortcut("kwin-wmplus(position8)",
+		"kwin-wmplus(position8)",
+		"Meta+Alt+8",
+		function() {
+			var clients = workspace.clientList();
+			for (var i=0; i<clients.length; i++) {
+				if(clients[i].active) {
+					activeClient = clients[i];
+					var workGeo = workspace.clientArea(KWin.PlacementArea, activeClient.screen, 1);
+					var geo = activeClient.geometry;
+					geo.x = (workGeo.width - geo.width) / 2;
+					geo.y = 0;
+					activeClient.geometry = geo;
+				}
+			}
+		});
+registerShortcut("kwin-wmplus(position3)",
+		"kwin-wmplus(position3)",
+		"Meta+Alt+3",
+		function() {
+			var clients = workspace.clientList();
+			for (var i=0; i<clients.length; i++) {
+				if(clients[i].active) {
+					activeClient = clients[i];
+					var workGeo = workspace.clientArea(KWin.PlacementArea, activeClient.screen, 1);
+					var geo = activeClient.geometry;
+					geo.x = workGeo.width - geo.width;
+					geo.y = workGeo.height - geo.height;
+					activeClient.geometry = geo;
+				}
+			}
+		});
+registerShortcut("kwin-wmplus(position6)",
+		"kwin-wmplus(position6)",
+		"Meta+Alt+6",
+		function() {
+			var clients = workspace.clientList();
+			for (var i=0; i<clients.length; i++) {
+				if(clients[i].active) {
+					activeClient = clients[i];
+					var workGeo = workspace.clientArea(KWin.PlacementArea, activeClient.screen, 1);
+					var geo = activeClient.geometry;
+					geo.x = workGeo.width - geo.width;
+					geo.y = (workGeo.height - geo.height) / 2;
+					activeClient.geometry = geo;
+				}
+			}
+		});
+registerShortcut("kwin-wmplus(position9)",
+		"kwin-wmplus(position9)",
+		"Meta+Alt+9",
+		function() {
+			var clients = workspace.clientList();
+			for (var i=0; i<clients.length; i++) {
+				if(clients[i].active) {
+					activeClient = clients[i];
+					var workGeo = workspace.clientArea(KWin.PlacementArea, activeClient.screen, 1);
+					var geo = activeClient.geometry;
+					geo.x = workGeo.width - geo.width;
+					geo.y = 0;
 					activeClient.geometry = geo;
 				}
 			}
