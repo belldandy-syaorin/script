2c855927aed6f40cb1d5703a9ab9f4d97042c1ae
Belldandy <Belldandy@Syaorin.com>
2015-11-29 01:13:31 +0900

    remove linux/temperature.sh

--- linux/temperature.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sensors | grep ^Physical
-sensors | grep ^Core
-sensors | grep ^fan
-sudo smartctl -a /dev/sda | grep -i temp
-sudo smartctl -a /dev/sdb | grep -i temp
-sudo smartctl -H /dev/sda | grep ^SMART
-sudo smartctl -H /dev/sdb | grep ^SMART
-exit 0
