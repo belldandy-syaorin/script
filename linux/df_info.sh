73078026e183a6de1bf030fc472d828c24bd5819
belldandy <belldandy@syaorin.com>
2016-05-17 05:53:17 +0900

    mod linux/df_info.sh

---
+++ linux/df_info.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ ! -d "$1" ] ; then
+	echo "[info] Not Directory & Exit"
+	exit 0
+fi
+echo "[info] Target Directory :" "$1"
+echo "[info] Directory & File :" $(ls -A1 $1 | wc -l)
+echo "[info] Directory :" $(ls -Al $1 | grep '^d' | wc -l)
+echo "[info] File :" $(ls -Al $1 | grep '^-' | wc -l)
+exit 0
