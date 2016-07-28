a2739b8b5ad1a9a4ca90790b219830a1f5774495
belldandy <belldandy@syaorin.com>
2016-07-28 20:36:06 +0900

    mod 7-zip/check_archive.sh

--- 7-zip/check_archive.sh
+++ 7-zip/check_archive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find . -regextype posix-extended -regex ".+\.(7z|bz2|exe|gz|msi|tgz|xz|zip)" -exec 7z t {} \; | grep -E '^Testing|^Everything' | xargs -n 6
+find . -regextype posix-extended -regex ".+\.(7z|bz2|exe|gz|msi|tgz|xpi|xz|zip)" -exec 7z t {} \; | grep -E '^Testing|^Everything' | xargs -n 6
 exit 0
