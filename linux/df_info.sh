dbf43518e6cf2fe10551000d6b540704b4f55c74
belldandy <belldandy@syaorin.com>
2016-08-11 22:11:27 +0900

    mod linux/df_info.sh

--- linux/df_info.sh
+++ linux/df_info.sh
@@ -4,8 +4,8 @@ if [ ! -d "$1" ] ; then
 	exit 0
 fi
 echo "[info] Target Directory :" "$1"
-echo "[info] Total            :" $(ls -A1 $1 | wc -l)
-echo "[info] Directory        :" $(ls -Al $1 | grep '^d' | wc -l)
-echo "[info] File             :" $(ls -Al $1 | grep '^-' | wc -l)
-echo "[info] Link             :" $(ls -Al $1 | grep '^l' | wc -l)
+echo "[info] Total            :" $(ls -A1 "$1" | wc -l)
+echo "[info] Directory        :" $(ls -Al "$1" | grep '^d' | wc -l)
+echo "[info] File             :" $(ls -Al "$1" | grep '^-' | wc -l)
+echo "[info] Link             :" $(ls -Al "$1" | grep '^l' | wc -l)
 exit 0
