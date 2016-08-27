3f07a97800b5e33f166efcbe0c65ff969e2baf79
belldandy <belldandy@syaorin.com>
2016-08-27 15:50:58 +0900

    mod iptables/ipset_update.sh

--- iptables/ipset_update.sh
+++ iptables/ipset_update.sh
@@ -1,38 +1,35 @@
 #!/bin/bash
-if [ -f /opt/ipset/cn.zone ] ; then
-	echo "[info] Update blacklistcn"
-	echo "[info] cn.zone :" $(wc -l /opt/ipset/cn.zone | awk '{print $1}')
+if [ -f /etc/sysconfig/ipset.conf ] ; then
+	savepath="/etc/sysconfig/ipset.conf"
+else
+	savepath="/etc/ipset.conf"
+fi
+cnpath="/opt/ipset/cn.zone"
+twpath="/opt/ipset/tw.zone"
+if [ -f $cnpath ] ; then
+	echo "[info] Create & Update blacklistcn"
+	echo "[info] cn.zone :" $(wc -l $cnpath | awk '{print $1}')
 	ipset -exist create blacklistcn hash:net
 	ipset create blacklistcn_update hash:net
-	for i in $(cat /opt/ipset/cn.zone) ; do ipset add blacklistcn_update $i ; done
+	for i in $(cat $cnpath) ; do ipset add blacklistcn_update $i ; done
 	ipset swap blacklistcn blacklistcn_update
 	ipset destroy blacklistcn_update
-	if [ -f /etc/sysconfig/ipset.conf ] ; then
-		ipset save > /etc/sysconfig/ipset.conf
-		echo "[info] ipset.conf :" $(wc -l /etc/sysconfig/ipset.conf | awk '{print $1}')
-	else
-		ipset save > /etc/ipset.conf
-		echo "[info] ipset.conf :" $(wc -l /etc/ipset.conf | awk '{print $1}')
-	fi
+	ipset save > $savepath
+	echo "[info] ipset.conf :" $(wc -l $savepath | awk '{print $1}')
 else
 	echo "[info] Can't Find cn.zone & Exit"
 	exit 0
 fi
-if [ -f /opt/ipset/tw.zone ] ; then
-	echo "[info] Update blacklisttw"
-	echo "[info] tw.zone :" $(wc -l /opt/ipset/tw.zone | awk '{print $1}')
+if [ -f $twpath ] ; then
+	echo "[info] Create & Update blacklisttw"
+	echo "[info] tw.zone :" $(wc -l $twpath | awk '{print $1}')
 	ipset -exist create blacklisttw hash:net
 	ipset create blacklisttw_update hash:net
-	for i in $(cat /opt/ipset/tw.zone) ; do ipset add blacklisttw_update $i ; done
+	for i in $(cat $twpath) ; do ipset add blacklisttw_update $i ; done
 	ipset swap blacklisttw blacklisttw_update
 	ipset destroy blacklisttw_update
-	if [ -f /etc/sysconfig/ipset.conf ] ; then
-		ipset save > /etc/sysconfig/ipset.conf
-		echo "[info] ipset.conf :" $(wc -l /etc/sysconfig/ipset.conf | awk '{print $1}')
-	else
-		ipset save > /etc/ipset.conf
-		echo "[info] ipset.conf :" $(wc -l /etc/ipset.conf | awk '{print $1}')
-	fi
+	ipset save > $savepath
+	echo "[info] ipset.conf :" $(wc -l $savepath | awk '{print $1}')
 else
 	echo "[info] Can't Find tw.zone & Exit"
 	exit 0
