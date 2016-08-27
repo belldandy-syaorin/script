c6881625d18ba06b41205bee1c8e1b37165d7d59
belldandy <belldandy@syaorin.com>
2016-08-27 23:24:39 +0900

    mod rhash/RHash_SFV.sh

--- rhash/RHash_SFV.sh
+++ rhash/RHash_SFV.sh
@@ -3,7 +3,7 @@ CHECKS=(ED2KSUMS MD5SUMS SHA1SUMS SHA256SUMS SHA512SUMS)
 for checks in ${CHECKS[@]}
 do
 	if [ -f ./$checks ] ; then
-		rhash --check $checks
+		rhash --check --percents $checks
 	fi
 done
 exit 0
