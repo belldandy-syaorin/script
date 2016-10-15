a13b70931353857342476487b7ce88dff2cc07fa
belldandy <belldandy@syaorin.com>
2016-10-15 22:05:01 +0900

    mod 7-zip/check_archive.sh

--- 7-zip/check_archive.sh
+++ 7-zip/check_archive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find . -maxdepth 1 -regextype posix-extended -regex ".+\.(7z|bz2|exe|gz|msi|tgz|xpi|xz|zip)" -exec 7z t {} \; | grep -E '^Testing|^Everything' | xargs -n 6 | sort
+find . -maxdepth 1 -regextype posix-extended -regex ".+\.(7z|bz2|exe|gz|msi|tgz|xpi|xz|zip)" -exec 7z t {} \; | grep -E '^Testing|^Everything' | awk '/^Testing/{i=$0 ; next ;}{print i " " $0 ;}' | sort
 exit 0
