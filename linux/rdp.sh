b929da2b4051b407db6dd61ee98da97d3a5a5e4e
belldandy <belldandy@syaorin.com>
2016-11-20 23:40:49 +0900

    mod linux/rdp.sh

--- linux/rdp.sh
+++ linux/rdp.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+if [[ ! $1 =~ ^192\.168(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){2}$ ]] ; then
+	echo "[info] Not 192.168.*.* & Exit"
+	exit 0
+fi
+echo "[info] xfreerdp +clipboard +fonts -themes -wallpaper /size:80% /v:$1"
 xfreerdp +clipboard +fonts -themes -wallpaper /size:80% /v:$1
 exit 0
