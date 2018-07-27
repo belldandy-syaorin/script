eb3094ca68a5487af572b3752fb92077dbced73a
belldandy <belldandy@syaorin.com>
2018-07-28 06:08:14 +0900

    remove vim/setting_*

--- vim/setting_neocomplcache_empty.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -d ~/.cache/neocomplcache ] ; then
-	rm -r ~/.cache/neocomplcache
-	echo "[ ok ] Find neocomplcache & Remove"
-else
-	echo "[info] Can't Find neocomplcache & Exit"
-fi
-exit 0
--- vim/setting_vim_empty.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -f ~/.viminfo ] ; then
-	rm ~/.viminfo
-	echo "[ ok ] Find .viminfo & Remove"
-else
-	echo "[info] Can't Find .viminfo & Exit"
-fi
-exit 0
