3f325c32d6722eda76b14dc6a11018e290beac9b
belldandy <belldandy@syaorin.com>
2018-10-02 21:03:58 +0900

    mod mpv/mpv.sh

--- mpv/mpv.sh
+++ mpv/mpv.sh
@@ -8,23 +8,25 @@ function argumentselect() {
 while [ -z "${argument[0]}" ]
 do
 echo "Input A Number For Your Choice (After 5s Will Auto Select Default)"
-echo "1 = --fullscreen (Default for mkv & mp4)"
-echo "2 = --audio-display=no --shuffle (Default for m3u8)"
-echo "3 = --vid=no --shuffle (Default for m3u)"
-echo "4 = --audio-display=no --loop-file=inf (Default for flac & mp3)"
-echo "5 = --vid=no --loop-file=inf"
-echo "6 = --window-scale=0.5"
-echo "7 = --window-scale=0.8"
+echo "1 = --audio-display=no --shuffle (Default for m3u8)"
+echo "2 = --vid=no --shuffle (Default for m3u)"
+echo "3 = --audio-display=no --loop-file=inf (Default for flac & mp3)"
+echo "4 = --vid=no --loop-file=inf"
+echo "5 = --audio-display=no"
+echo "6 = --vid=no"
+echo "7 = --fullscreen"
+echo "8 = --window-scale=0.5"
+echo "9 = --window-scale=0.8 (Default for mkv & mp4)"
 read -t 5 -p "Select Feature (0 = Exit) : " as
 if [ -z "$as" ] ; then
-	if [[ "$fne" =~ ^(mkv|mp4)$ ]] ; then
+	if [[ "$fne" =~ ^m3u8$ ]] ; then
 		as="1"
-	elif [[ "$fne" =~ ^m3u8$ ]] ; then
-		as="2"
 	elif [[ "$fne" =~ ^m3u$ ]] ; then
-		as="3"
+		as="2"
 	elif [[ "$fne" =~ ^(flac|mp3)$ ]] ; then
-		as="4"
+		as="3"
+	elif [[ "$fne" =~ ^(mkv|mp4)$ ]] ; then
+		as="9"
 	else
 		as="0"
 	fi
@@ -36,28 +38,34 @@ if [[ "$as" =~ [0-9] ]] ; then
 			exit 0
 		;;
 		"1")
-			argument[0]="--fullscreen"
+			argument[0]="--audio-display=no"
+			argument[1]="--shuffle"
 		;;
 		"2")
-			argument[0]="--audio-display=no"
+			argument[0]="--vid=no"
 			argument[1]="--shuffle"
 		;;
 		"3")
-			argument[0]="--vid=no"
-			argument[1]="--shuffle"
-		;;
-		"4")
 			argument[0]="--audio-display=no"
 			argument[1]="--loop-file=inf"
 		;;
-		"5")
+		"4")
 			argument[0]="--vid=no"
 			argument[1]="--loop-file=inf"
 		;;
+		"5")
+			argument[0]="--audio-display=no"
+		;;
 		"6")
-			argument[0]="--window-scale=0.5"
+			argument[0]="--vid=no"
 		;;
 		"7")
+			argument[0]="--fullscreen"
+		;;
+		"8")
+			argument[0]="--window-scale=0.5"
+		;;
+		"9")
 			argument[0]="--window-scale=0.8"
 		;;
 	esac
