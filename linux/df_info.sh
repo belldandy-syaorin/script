e3bad3fcf2ff0d0a0efa0e518089747d7a4c1595
belldandy <belldandy@syaorin.com>
2018-11-01 04:28:30 +0900

    mod linux/df_info.sh & mpv/mpv.sh

--- linux/df_info.sh
+++ linux/df_info.sh
@@ -2,10 +2,12 @@
 if [ ! -d "$1" ] ; then
 	echo "[info] Not Directory & Exit"
 	exit 0
+else
+	fl=$(ls -Al "$1")
 fi
 echo "[info] Target Directory :" "$1"
-echo "[info] Total            :" $(ls -A1 "$1" | wc -l)
-echo "[info] Directory        :" $(ls -Al "$1" | grep '^d' | wc -l)
-echo "[info] File             :" $(ls -Al "$1" | grep '^-' | wc -l)
-echo "[info] Link             :" $(ls -Al "$1" | grep '^l' | wc -l)
+echo "[info] Total            :" $(echo "${fl[@]}" | grep -v '^total' | wc -l)
+echo "[info] Directory        :" $(echo "${fl[@]}" | grep '^d' | wc -l)
+echo "[info] File             :" $(echo "${fl[@]}" | grep '^-' | wc -l)
+echo "[info] Link             :" $(echo "${fl[@]}" | grep '^l' | wc -l)
 exit 0
--- mpv/mpv.sh
+++ mpv/mpv.sh
@@ -3,30 +3,51 @@ declare -a argument
 fn=$(basename "$1")
 fne="${fn##*.}"
 fnl=("$@")
+privatepl="$HOME/.config/mpv/privatepl"
+if [ -z "$1" ] ; then
+	if [ ! -f "$privatepl" ] ; then
+		echo "[info] Can't Find playlist & Exit"
+		exit 0
+	else
+		readarray -t fnl < "$privatepl"
+	fi
+fi
 
 function argumentselect() {
 while [ -z "${argument[0]}" ]
 do
 echo "[info] Input A Number For Your Choice (After 5s Will Auto Select Default)"
-echo "1 = --audio-display=no --shuffle (Default for m3u8)"
+echo "1 = --audio-display=no --shuffle --volume=50 (Default for m3u8)"
 echo "2 = --vid=no --shuffle (Default for m3u)"
-echo "3 = --audio-display=no --loop-file=inf (Default for flac & mp3)"
+echo "3 = --audio-display=no --loop-file=inf --volume=50 (Default for flac & mp3 & privatepl)"
 echo "4 = --vid=no --loop-file=inf"
-echo "5 = --audio-display=no"
-echo "6 = --vid=no"
+echo "5 = --audio-display=no --loop-playlist=inf --volume=50 (Default for multiple file (flac & mp3))"
+echo "6 = --vid=no --loop-playlist=inf (Default for multiple file (mkv & mp4))"
 echo "7 = --fullscreen"
-echo "8 = --window-scale=0.5"
-echo "9 = --window-scale=0.8 (Default for mkv & mp4)"
+echo "8 = --window-scale=0.5 (Default for mkv & mp4 (1920x1080))"
+echo "9 = --window-scale=0.8 (Default for mkv & mp4 (1280x720))"
 read -t 5 -p "Select Feature (0 = Exit) : " as
 if [ -z "$as" ] ; then
-	if [[ "$fne" =~ ^m3u8$ ]] ; then
-		as="1"
-	elif [[ "$fne" =~ ^m3u$ ]] ; then
-		as="2"
-	elif [[ "$fne" =~ ^(flac|mp3)$ ]] ; then
+	if [ -z "$fn" ] ; then
 		as="3"
-	elif [[ "$fne" =~ ^(mkv|mp4)$ ]] ; then
-		as="9"
+	elif [ "${#fnl[@]}" -eq 1 ] ; then
+		if [[ "$fne" =~ ^m3u8$ ]] ; then
+			as="1"
+		elif [[ "$fne" =~ ^m3u$ ]] ; then
+			as="2"
+		elif [[ "$fne" =~ ^(flac|mp3)$ ]] ; then
+			as="3"
+		elif [[ "$fn" =~ ^.*1920x1080.*$ ]] && [[ "$fne" =~ ^(mkv|mp4)$ ]] ; then
+			as="8"
+		elif [[ "$fn" =~ ^.*1280x720.*$ ]] && [[ "$fne" =~ ^(mkv|mp4)$ ]] ; then
+			as="9"
+		fi
+	elif [ "${#fnl[@]}" -ge 2 ] ; then
+		if [[ "$fne" =~ ^(flac|mp3)$ ]] ; then
+			as="5"
+		elif [[ "$fne" =~ ^(mkv|mp4)$ ]] ; then
+			as="6"
+		fi
 	else
 		as="0"
 	fi
@@ -40,6 +61,7 @@ if [[ "$as" =~ [0-9] ]] ; then
 		"1")
 			argument[0]="--audio-display=no"
 			argument[1]="--shuffle"
+			argument[2]="--volume=50"
 		;;
 		"2")
 			argument[0]="--vid=no"
@@ -48,6 +70,7 @@ if [[ "$as" =~ [0-9] ]] ; then
 		"3")
 			argument[0]="--audio-display=no"
 			argument[1]="--loop-file=inf"
+			argument[2]="--volume=50"
 		;;
 		"4")
 			argument[0]="--vid=no"
@@ -55,9 +78,12 @@ if [[ "$as" =~ [0-9] ]] ; then
 		;;
 		"5")
 			argument[0]="--audio-display=no"
+			argument[1]="--loop-playlist=inf"
+			argument[2]="--volume=50"
 		;;
 		"6")
 			argument[0]="--vid=no"
+			argument[1]="--loop-playlist=inf"
 		;;
 		"7")
 			argument[0]="--fullscreen"
@@ -73,22 +99,24 @@ fi
 done
 }
 
-if [ -z "$1" ] ; then
-	echo "[info] argument : --player-operation-mode=pseudo-gui --window-scale=0.5"
-	mpv --player-operation-mode=pseudo-gui --window-scale=0.5 &
-	exit 0
+argumentselect
+if [ ! -z "${argument[2]}" ] && [[ "$fne" =~ ^flac$ ]] ; then
+	argument[2]="--volume=75"
+fi
+if [ -z "${argument[1]}" ] ; then
+	echo "[info] playlist : ${#fnl[@]}"
+	printf '\t%s\n' "${fnl[@]}"
+	echo "[info] argument : ${argument[0]}"
+	mpv "${fnl[@]}" "${argument[0]}"
+elif [ -z "${argument[2]}" ] ; then
+	echo "[info] playlist : ${#fnl[@]}"
+	printf '\t%s\n' "${fnl[@]}"
+	echo "[info] argument : ${argument[0]} ${argument[1]}"
+	mpv "${fnl[@]}" "${argument[0]}" "${argument[1]}"
 else
-	argumentselect
-	if [ -z "${argument[1]}" ] ; then
-		echo "[info] playlist :"
-		printf '\t%s\n' "${fnl[@]}"
-		echo "[info] argument : ${argument[0]}"
-		mpv "${fnl[@]}" "${argument[0]}"
-	else
-		echo "[info] playlist :"
-		printf '\t%s\n' "${fnl[@]}"
-		echo "[info] argument : ${argument[0]} ${argument[1]}"
-		mpv "${fnl[@]}" "${argument[0]}" "${argument[1]}"
-	fi
+	echo "[info] playlist : ${#fnl[@]}"
+	printf '\t%s\n' "${fnl[@]}"
+	echo "[info] argument : ${argument[0]} ${argument[1]} ${argument[2]}"
+	mpv "${fnl[@]}" "${argument[0]}" "${argument[1]}" "${argument[2]}"
 fi
 exit 0
