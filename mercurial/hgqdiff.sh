31f90fa7e275ea0d6e2472d136c1616aa264f36b
belldandy <belldandy@syaorin.com>
2019-03-18 22:48:18 +0900

    mod mercurial/hgqdiff.sh

--- mercurial/hgqdiff.sh
+++ mercurial/hgqdiff.sh
@@ -11,9 +11,14 @@ fi
 echo "[info] Repository Log(Last 2)"
 hg log --graph --limit 2 --stat
 echo "[info] Repository Changeset(Last 2) Diff :" "${argument[1]}" "${argument[2]}"
-echo "k = KDiff3 , v = VIM (After 5s Will Auto Exit)"
+echo "g = GVIM , k = KDiff3 , v = VIM (After 5s Will Auto Exit)"
 read -t 5 -p "Select Feature (Not k/v or No Input = Exit) : " ans
 case $ans in
+	"g")
+		echo "[info] GVIM"
+		hg gvimdiff -r"${argument[1]}" -r"${argument[2]}"
+		exit 0
+	;;
 	"k")
 		echo "[info] KDiff3"
 		hg kdiff3 -r"${argument[1]}" -r"${argument[2]}"
