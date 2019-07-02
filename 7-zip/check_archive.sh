dbee95f6c18cdb171fc32fe5d0ced5bbcd34d2d9
belldandy <belldandy@syaorin.com>
2019-07-03 04:11:04 +0900

    7-zip/check_archive.sh

--- 7-zip/check_archive.sh
+++ 7-zip/check_archive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find . -maxdepth 1 -type f -regextype posix-extended -regex ".+\.(7z|bz2|exe|gz|iso|msi|tar|tgz|xpi|zip)" -exec 7z t {} \; | grep -E '^Testing|^Everything' | awk '/^Testing/{i=$0 ; j++ ; next ;}{print "[info] " i " " $0 ;}END{print "[info] Total : " j}' | sort
+find . -maxdepth 1 -type f -regextype posix-extended -regex ".+\.(7z|bz2|exe|gz|iso|msi|rar|tar|tgz|xpi|zip)" -exec 7z t {} \; | grep -E '^Testing|^Everything' | awk '/^Testing/{i=$0 ; j++ ; next ;}{print "[info] " i " " $0 ;}END{print "[info] Total : " j}' | sort
 exit 0
