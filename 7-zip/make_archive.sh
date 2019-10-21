1a5a0e859dd45c4cdd6113e41e62bac5778ef28f
belldandy <belldandy@syaorin.com>
2019-10-21 22:26:44 +0900

    mod 7-zip/make_archive.sh

--- 7-zip/make_archive.sh
+++ 7-zip/make_archive.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 DATE=$(date +"%Y.%m.%d")
+DATETIME=$(date +"%Y.%m.%d-%H%M")
 fn=$(basename "$1")
-fna=""${fn%.*}"("${DATE}").7z"
-7z a "${fna}" "$1"
+fnad=""${fn%.*}"("${DATE}").7z"
+fnadt=""${fn%.*}"("${DATETIME}").7z"
+if [ ! -f "${fnad}" ] ; then
+	7z a "${fnad}" "$1"
+else
+	7z a "${fnadt}" "$1"
+fi
 exit 0
