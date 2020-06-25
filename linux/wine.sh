53fb701f67473a3b88fc8e05725901a48acaf99a
belldandy <belldandy@syaorin.com>
2020-06-26 05:42:33 +0900

    mod linux/wine.sh

--- linux/wine.sh
+++ linux/wine.sh
@@ -4,10 +4,10 @@ if [[ ! "$1" =~ ^.+\.(exe|EXE)$ ]] ; then
 	exit 0
 fi
 if [[ "$2" =~ ^fps$ ]] ; then
-	DXVK_HUD=fps LANG=ja_JP.UTF-8 WINEDLLOVERRIDES="d3d10_1,d3d10core,d3d10,d3d11,d3d9,dxgi=n" wine $1
+	DXVK_HUD=fps LANG=ja_JP.UTF-8 WINEDLLOVERRIDES="d3d10_1,d3d10core,d3d10,d3d11,d3d9,dxgi=n" wine "$1"
 elif [[ "$2" =~ ^full$ ]] ; then
-	DXVK_HUD=full LANG=ja_JP.UTF-8 WINEDLLOVERRIDES="d3d10_1,d3d10core,d3d10,d3d11,d3d9,dxgi=n" wine $1
+	DXVK_HUD=full LANG=ja_JP.UTF-8 WINEDLLOVERRIDES="d3d10_1,d3d10core,d3d10,d3d11,d3d9,dxgi=n" wine "$1"
 else
-	LANG=ja_JP.UTF-8 wine $1
+	LANG=ja_JP.UTF-8 wine "$1"
 fi
 exit 0
