9d0a557f19215cf5f09146686238b50057315b7a
belldandy <belldandy@syaorin.com>
2020-07-11 02:37:00 +0900

    mod linux/df_info.sh

--- linux/df_info.sh
+++ linux/df_info.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-if [ ! -d "$1" ] ; then
-	echo "[info] Not Directory & Exit"
-	exit 0
-else
-	fl=$(ls -Al "$1")
-fi
-echo "[info] Target Directory :" "$1"
-echo "[info] Directory        :" $(echo "${fl[@]}" | grep '^d' | wc -l)
-echo "[info] File             :" $(echo "${fl[@]}" | grep '^-' | wc -l)
-echo "[info] Link             :" $(echo "${fl[@]}" | grep '^l' | wc -l)
+for dirs in $@
+do
+	if [ ! -d "$dirs" ] ; then
+		echo "[info] Not Directory & Exit"
+		exit 0
+	else
+		fl=$(ls -Al "$dirs")
+	fi
+	echo "[info] Target Directory :" "$dirs"
+	echo "[info] Directory        :" $(echo "${fl[@]}" | grep '^d' | wc -l)
+	echo "[info] File             :" $(echo "${fl[@]}" | grep '^-' | wc -l)
+	echo "[info] Link             :" $(echo "${fl[@]}" | grep '^l' | wc -l)
+done
 exit 0
